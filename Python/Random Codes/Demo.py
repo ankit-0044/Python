5e59620d6cf47095477684176ed74f7ff32f3bf1
ankit-0044 <ankitsinghshakya.as@gmail.com>
2022-09-04 21:42:32 +0530

    Random

--- Python/Random Codes/Demo.py
+++ Python/Random Codes/Demo.py
@@ -1,38 +1,55 @@
+# Import module
+from geopy.geocoders import Nominatim
 import os
 import glob
 import pandas as pd
+from time import sleep
+import reverse_geocoder as rg
 
-def restart_csv():
-    try:
-        path = input("Enter path: ")
-        Section = input("Enter section: ")
-        print("Yhn tak sab shi hai")
+# getting folder location input
+# Folder_path = input(r'Enter 1st Row CSV Folder Path :: ')
 
-        all_files = glob.glob(os.path.join(path, "Makgn*.csv"))
-        print("Sari Files ye hai",all_files)
-        frame = pd.DataFrame()
-        list_ = []
-        print("Loop se phle",list_)
-        for file_ in all_files:
-            df = pd.read_csv(file_, nrow =1, low_memory=False)
-            list_.append(df)
-            print("Loop k andr",list_)
-        print("Loop k baad",list_)
-        if list_ == []:
-            print("List khali hai")
-        else:
-            frame = pd.concat(list_)
-            frame.to_csv(path + "csv file" + Section + ".csv", index=False)
+# # read all the files in the folder
+# all_files = glob.glob(os.path.join(Folder_path, "1ST ROW FILE CSV*.csv"))
+# for file_ in all_files:
 
-        print("CSV bn gyi")
-        return True
-    except:
-        print("Koi dikkat hai")
-        return True
-while True:
-    check = input("Enter Y to continue: ")
-    if check == 'y':
-        restart_csv()
-    else:
-        print('Exiting..')
-        break
+#         file_name = file_.split("\\")      # split file name
+#         print("Working on File: ",file_name[-1])
+
+#         # read the CSV files
+#         df = pd.read_csv(file_, encoding='utf-8', low_memory=False)
+
+#         # Get latitude and longitude from CSV
+#         coordinates = list(zip(df['latitude'],df['longitude']))
+
+
+
+# Initialize Nominatim API
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+Latitude = "25.594095"
+Longitude = "85.137566"
+
+
+# Get location with geocode
+location = geolocator.reverse(Latitude+","+Longitude, addressdetails=True)
+
+data = location.raw
+#print(data)
+data = data['address']
+address = str(data)
+print(address)
+
+
+# street = district = postalCode= state = country = countryCode = ""
+
+# district    =str(data['state_district'])
+# postalCode  =str(data['postcode'])
+# state       =str(data['state'])
+# country     =str(data['country'])
+# countryCode =str(data['country_code']).upper()
+
+# address = street +' '+ district  +' '+  postalCode  +' '+  state  +' '+  country  +' '+  countryCode
+# print("Address:",address)
+# print("\nLocation of the given Latitude and Longitude:")
+# print("All Details: ",location)
